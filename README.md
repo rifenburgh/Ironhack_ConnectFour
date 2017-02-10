1fdcbcd147fe450a6779c4a70e9a9b9b9c8e039f
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-02-10 13:56:25 -0500

    HTML & CSS

---
+++ README.md
@@ -0,0 +1,4 @@
+#Connect Four
+
+Student project to recreate the Connect Four game
+Ironhack Module 1 - Week 2
--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+console.log("app.js connected.");
 
 $("document").ready(function() {
 
-
 });
+
+function ConnectFour(playerName1, playerName2) {
+  this.player1 = playerName1;
+  this.player2 = playerName2;
+  this.currentPlayer;
+  this.winner = null;
+  this.board = [
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]
+  ];
+  if (Math.random() < 0.5) {
+    this.currentPlayer = this.player1;
+  } else {
+    this.currentPlayer = this.player2;
+  }
+  console.log(this.currentPlayer);
+
+};
+ConnectFour.prototype.currentPlayer = function() {
+};
+var globalVariable = "New Connect Four Game";
+
+
+var newGame = new ConnectFour("Bob", "Sam");
