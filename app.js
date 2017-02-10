8dceba4b8b33eef1403aac9bea9f59f2c86b4669
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-02-10 15:45:44 -0500

    Drawing the board from an array

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 console.log("app.js connected.");
 
+//!! Wrap functions in document ready upon code completion !!
 $("document").ready(function() {
-
 });
+
+var myGlobalConnectGame;
+var globalVariable = "New Connect Four Game";
 
 function ConnectFour(playerName1, playerName2) {
   this.player1 = playerName1;
   this.player2 = playerName2;
-  this.currentPlayer;
+  this.currentPlayer = 1;
   this.winner = null;
   this.board = [
-    [null, null, null, null, null, null, null],
+    [1, 2, null, null, null, null, null],
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
@@ -24,10 +27,27 @@ function ConnectFour(playerName1, playerName2) {
   }
   console.log(this.currentPlayer);
 
+}
+
+ConnectFour.prototype.renderBoard = function() {
+  $('#board').empty();
+  this.board.forEach(function(row, rowIndex) {
+    row.forEach(function(slot, colIndex) {
+      var playerNum;
+      if (slot === 1) {
+        //player1 checker
+        playerNum = 'player-checker-1';
+      } else if (slot === 2){
+        playerNum = 'player-checker-2';
+      } else {
+        playerNum = 'player-checker-0';
+      }
+      var slotHtml = '<div class="square"><div class="slot ' + playerNum + '"></div></div>';
+      $("#board").append(slotHtml);
+    });
+  });
 };
-ConnectFour.prototype.currentPlayer = function() {
-};
-var globalVariable = "New Connect Four Game";
 
 
 var newGame = new ConnectFour("Bob", "Sam");
+newGame.renderBoard();
